060e1f0e976b0b66c2a02134619d83854c98d8a9
Ergün Karakuş <EKarakus@btofis.com>
2025-05-22 12:40:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/miniapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.miniapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.miniapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btn1.setOnClickListener(v -> {
+            startActivity(new Intent(this, HakkimizdaActivity.class));
+        });
+
+        binding.btn2.setOnClickListener(v -> {
+            startActivity(new Intent(this, IletisimActivity.class));
+        });
+
+
+    }
+}
